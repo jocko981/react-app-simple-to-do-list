9cbe044a011368a8e2e2504136aada54bb6628f6
Jovan <jovannikolic17@gmail.com>
2021-05-12 01:30:46 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,14 +4,16 @@ import InputArea from "./InputArea";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [msg, setMsg] = useState(null);
 
   function addItem(inputText) {
-    // if (!inputText) {
-    //   return null;
-    // }
+    if (!inputText) {
+      return setMsg("You must type at least One character");
+    }
     setItems((prevItems) => {
       return [...prevItems, inputText];
     });
+    setMsg("");
   }
 
   function deleteItem(id) {
@@ -28,6 +30,7 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <InputArea onAdd={addItem} />
+      <p>{msg}</p>
       <div>
         <ul>
           {items.map((todoItem, index) => (
